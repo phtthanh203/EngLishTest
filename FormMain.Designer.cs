abf6de16c0f9a7218ef215e154028d0c9f937ee8
phước em <phuocanh554@gmail.com>
2025-04-20 09:06:07 +0700

    Lưu tạm chỉnh sửa FormMain.Designer.cs

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -1,86 +1,128 @@
-﻿namespace EnglishTest
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace EnglishTest
 {
     partial class FormMain
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox2;
+        private Button button2;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new Label();
+            this.label2 = new Label();
+            this.label3 = new Label();
+            this.comboBox1 = new ComboBox();
+            this.label4 = new Label();
+            this.comboBox2 = new ComboBox();
+            this.button2 = new Button();
+            this.button1 = new Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(255, 284);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(300, 93);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Quan Ly Cau Hoi";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(58, 112);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(300, 93);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Tieng Anh";
+
+
+            this.ClientSize = new Size(650, 400);
+            this.BackColor = Color.WhiteSmoke;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "🎓 English Test App";
+            this.Load += new EventHandler(this.FormMain_Load);
+
+
+            this.label1.Font = new Font("Segoe UI", 28F, FontStyle.Bold);
+            this.label1.ForeColor = Color.FromArgb(30, 144, 255);
+            this.label1.Location = new Point(0, 20);
+            this.label1.Size = new Size(650, 70);
+            this.label1.Text = "✨ Welcome to Quizz Test ✨";
+            this.label1.TextAlign = ContentAlignment.MiddleCenter;
+
+            this.label2.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            this.label2.Location = new Point(180, 90);
+            this.label2.Size = new Size(290, 35);
+            this.label2.Text = "🎯 Select Your Mode";
+            this.label2.TextAlign = ContentAlignment.MiddleCenter;
+
+            this.label3.Font = new Font("Segoe UI", 12F);
+            this.label3.ForeColor = Color.Black;
+            this.label3.Location = new Point(60, 145);
+            this.label3.Size = new Size(110, 25);
+            this.label3.Text = "🌐 Language:";
+            this.label3.TextAlign = ContentAlignment.MiddleLeft;
+
+            this.comboBox1.Font = new Font("Segoe UI", 12F);
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBox1.Location = new Point(175, 142);
+            this.comboBox1.Size = new Size(170, 35);
+            this.comboBox1.BackColor = Color.WhiteSmoke;
+            this.comboBox1.ForeColor = Color.Black;
+            this.comboBox1.FlatStyle = FlatStyle.Standard;
+
+            this.label4.Font = new Font("Segoe UI", 12F);
+            this.label4.Location = new Point(380, 145);
+            this.label4.Size = new Size(80, 25);
+            this.label4.Text = "📊 Level:";
+            this.label4.TextAlign = ContentAlignment.MiddleLeft;
+
+
+            this.comboBox2.Font = new Font("Segoe UI", 12F);
+            this.comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBox2.Location = new Point(460, 142);
+            this.comboBox2.Size = new Size(120, 35);
+            this.comboBox2.BackColor = Color.WhiteSmoke;
+            this.comboBox2.ForeColor = Color.Black;
+            this.comboBox2.FlatStyle = FlatStyle.Standard;
+
+            this.button2.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            this.button2.BackColor = Color.FromArgb(0, 123, 255);
+            this.button2.ForeColor = Color.White;
+            this.button2.FlatStyle = FlatStyle.Flat;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new Point(205, 230);
+            this.button2.Size = new Size(240, 60);
+            this.button2.Text = "🚀 START QUIZ";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(449, 112);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(300, 93);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Tieng Viet";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
+            this.button2.Click += new EventHandler(this.button2_Click);
+            this.button2.MouseEnter += (s, e) => button2.BackColor = Color.FromArgb(0, 102, 204);
+            this.button2.MouseLeave += (s, e) => button2.BackColor = Color.FromArgb(0, 123, 255);
+
+            this.button1.Font = new Font("Segoe UI", 10F);
+            this.button1.BackColor = Color.FromArgb(80, 80, 80);
+            this.button1.ForeColor = Color.White;
+            this.button1.FlatStyle = FlatStyle.Flat;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new Point(20, 340);
+            this.button1.Size = new Size(160, 40);
+            this.button1.Text = "🛠 Manage Questions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new EventHandler(this.button1_Click);
+            this.button1.MouseEnter += (s, e) => button1.BackColor = Color.FromArgb(60, 60, 60);
+            this.button1.MouseLeave += (s, e) => button1.BackColor = Color.FromArgb(80, 80, 80);
+
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "FormMain";
-            this.Text = "FormMain";
-            this.Load += new System.EventHandler(this.FormMain_Load);
+
             this.ResumeLayout(false);
-
         }
-
-        #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
